de983dd78fcaf47eb46686c88c8afeb57bb78917
xly <ly763247862@163.com>
2020-02-11 12:39:25 +0800

    第一次提交

---
+++ src/main/java/club/xlydbb/springbootfastdfs/SpringbootFastdfsApplication.java
@@ -0,0 +1,15 @@
+package club.xlydbb.springbootfastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class SpringbootFastdfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFastdfsApplication.class, args);
+	}
+
+}
